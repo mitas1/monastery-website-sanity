aa595170805c7213c5612006c7eb7aa8f88bd948
Samuel Mitas <samuel.mitas@devinsolutions.com>
2019-10-22 23:12:46 +0200

    Create base announcement schema

--- schemas/author.js
+++ schemas/author.js
@@ -1,48 +1,35 @@
 export default {
-  name: 'author',
-  title: 'Author',
-  type: 'document',
-  fields: [
-    {
-      name: 'name',
-      title: 'Name',
-      type: 'string'
-    },
-    {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'name',
-        maxLength: 96
-      }
-    },
-    {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
-      options: {
-        hotspot: true
-      }
-    },
-    {
-      name: 'bio',
-      title: 'Bio',
-      type: 'array',
-      of: [
+    name: "author",
+    title: "Author",
+    type: "document",
+    fields: [
         {
-          title: 'Block',
-          type: 'block',
-          styles: [{title: 'Normal', value: 'normal'}],
-          lists: []
-        }
-      ]
-    }
-  ],
-  preview: {
-    select: {
-      title: 'name',
-      media: 'image'
-    }
-  }
-}
+            name: "name",
+            title: "Name",
+            type: "string",
+        },
+        {
+            name: "slug",
+            title: "Slug",
+            type: "slug",
+            options: {
+                source: "name",
+                maxLength: 96,
+            },
+        },
+        {
+            name: "image",
+            title: "Image",
+            type: "image",
+            options: {
+                hotspot: true,
+            },
+        },
+    ],
+    preview: {
+        select: {
+            title: "name",
+            media: "image",
+        },
+    },
+};
